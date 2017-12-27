141c26bf75fa8ee15584dca3d320f3741f069499
xichijun <xicj@wiseonline.com.cn>
2017-12-27 17:11:52 +0800

    初始化项目

---
+++ index.rst
@@ -0,0 +1,19 @@
+示例
+======
+
+
+目录:
+
+.. toctree::
+    :maxdepth: 2
+
+    src/sample.rst
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setting.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- 
+
+config = {
+    "auther": u"奚驰俊",
+    "project": u"test",
+    "version": u"0.1",
+    "copyright": u"test",
+    "pdf_filename": u"test",
+    "pdf_title": u"test",
+    "html_title": u"testdoc",
+}
